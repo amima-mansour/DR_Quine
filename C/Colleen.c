562b337eef309460afcc76216de1d55e14a203d7
Amina MANSOUR <amansour@e2r1p8.42.fr>
2019-10-04 10:05:32 +0200

    Colleen (c) fixed

---
+++ C/Colleen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+	Hello program.
+*/
+void print(char *s)
+{
+	printf(s,10,10,9,10,10,10,10,9,10,10,10,10,10,9,10,10,9,34,s,34,10,9,10,9,10,10);
+}
+int main()
+{
+/*
+	Hello main.
+*/
+	char *s = "#include <stdio.h>%c/*%c%cHello program.%c*/%cvoid print(char *s)%c{%c%cprintf(s,10,10,9,10,10,10,10,9,10,10,10,10,10,9,10,10,9,34,s,34,10,9,10,9,10,10);%c}%cint main()%c{%c/*%c%cHello main.%c*/%c%cchar *s = %c%s%c;%c%cprint(s);%c%creturn (0);%c}%c";
+	print(s);
+	return (0);
+}
