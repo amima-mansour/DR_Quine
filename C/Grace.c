1f54f714816f611d6baeae76c418b974996f976f
Amina MANSOUR <amansour@e2r1p8.42.fr>
2019-10-06 12:36:00 +0200

    pb Grace fixed

--- C/Grace.c
+++ C/Grace.c
@@ -2,7 +2,8 @@
 	Hello program.
 */
 #include <stdio.h>
-#define FD fopen("Grace_kid.c", "w")
-#define STR "/*%c%cHello program.%c*/%c#include <stdio.h>%c#define FD fopen(%cGrace_kid.c%c, %cw%c)%c#define STR %c%s%c%c#define FT(x)int main(){fprintf(x,STR,10,9,10,10,10,34,34,34,34,10,34,STR,34,10,10,10);fclose(x);}%cFT(FD);%c"
-#define FT(x)int main(){fprintf(x,STR,10,9,10,10,10,34,34,34,34,10,34,STR,34,10,10,10);fclose(x);}
-FT(FD);
+#include <stdlib.h>
+#define FP fopen("Grace_kid.c", "w")
+#define STR "/*%c%cHello program.%c*/%c#include <stdio.h>%c#include <stdlib.h>%c#define FP fopen(%cGrace_kid.c%c, %cw%c)%c#define STR %c%s%c%c#define FT(x)int main(){if (FP == NULL)exit(-1);fprintf(x,STR,10,9,10,10,10,10,34,34,34,34,10,34,STR,34,10,10,10);fclose(x);}%cFT(FP);%c"
+#define FT(x)int main(){if (FP == NULL)exit(-1);fprintf(x,STR,10,9,10,10,10,10,34,34,34,34,10,34,STR,34,10,10,10);fclose(x);}
+FT(FP);
