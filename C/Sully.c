eee25f7545c52133b9f23cedc6794e7b7d649ebf
Amina MANSOUR <amansour@e2r1p8.42.fr>
2019-10-07 14:40:03 +0200

    encore pb en Sully.c

--- C/Sully.c
+++ C/Sully.c
@@ -3,7 +3,7 @@
 int main()
 {
 	int i = 5;
-	char *s = "#include <stdio.h>%c#include <stdlib.h>%cint main()%c{%c%cint i = %d;%c%cchar *s = %c%s%c;%c%cchar name[10] = %cSully_5.c%c;%c%cchar exec[10] = %c./Sully_5%c;%c%cFILE *fp;%c%cchar cmd[50] = %cgcc -Wall -Wextra -Werror -o Sully_5 Sully_5.c%c;%c%cwhile (i >= 0)%c%c{%c%c%cfp = fopen(name,%cw%c);%c%c%cif (!fp) exit(1);%c%c%cfprintf(fp,s,10,10,10,10,9,i,10,9,34,s,34,10,9,34,34,10,9,34,34,10,9,10,9,34,34,10,9,10,9,10,9,9,34,34,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,10,10);%c%c%cfclose(fp);%c%c%c--i;%c%c%cname[6] -= 1;%c%c%csystem(cmd);%c%c%csystem(exec);%c%c%ccmd[35] -= 1;%c%c%ccmd[43] -= 1;%c%c%cexec[8] -= 1;%c%c}%c}%c";
+	char *s = "#include <stdio.h>%c#include <stdlib.h>%cint main()%c{%c%cint i = %d;%c%cchar *s = %c%s%c;%c%cchar name[10] = %cSully_5.c%c;%c%cchar exec[10] = %c./Sully_5%c;%c%cFILE *fp;%c%cchar cmd[50] = %cgcc -Wall -Wextra -Werror -o Sully_5 Sully_5.c%c;%c%cwhile (i >= 0)%c%c{%c%c%cfp = fopen(name,%cw%c);%c%c%cif (!fp) exit(1);%c%c%cfprintf(fp,s,10,10,10,10,9,i,10,9,34,s,34,10,9,34,34,10,9,34,34,10,9,10,9,34,34,10,9,10,9,10,9,9,34,34,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,9,10,9,10,10);%c%c%cfclose(fp);%c%c%c--i;%c%c%cname[6] -= 1;%c%c%csystem(cmd);%c%c%csystem(exec);%c%c%ccmd[35] -= 1;%c%c%ccmd[43] -= 1;%c%c%cexec[8] -= 1;%c%c}%c}%c";
 	char name[10] = "Sully_5.c";
 	char exec[10] = "./Sully_5";
 	FILE *fp;
@@ -20,6 +20,6 @@ int main()
 		system(exec);
 		cmd[35] -= 1;
 		cmd[43] -= 1;
-        exec[8] -= 1;
-    }
-}
+		exec[8] -= 1;
+	}
+}
