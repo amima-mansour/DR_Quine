d58d6a2127579dd38d5405dceed387744cc44a66
Amina MANSOUR <amansour@e1r2p23.42.fr>
2019-10-09 13:50:35 +0200

    Sully.c fixed

--- C/Sully.c
+++ C/Sully.c
@@ -5,10 +5,11 @@
 int main()
 {
 	int i = 5;
-	if (open("Sully_5.c", O_RDONLY) != -1) {--i;}
-	char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$cint main()%1$c{%1$c%2$cint i = %3$d;%1$c%2$cif (open(%4$cSully_5.c%4$c, O_RDONLY) != -1) {--i;}%1$c%2$cchar *s = %4$c%5$s%4$c;%1$c%2$cint fd;%1$c%2$cchar name[10];%1$c%2$csprintf(name,%4$cSully_%6$cd.c%4$c,i);%1$c%2$cfd = open(name,O_RDWR | O_CREAT, 00644);%1$c%2$cif (fd == -1) exit(1);%1$c%2$cdprintf(fd,s,10,9,i,34,s,37);%1$c%2$cclose(fd);%1$c%2$cchar cmd[100];%1$c%2$csprintf(cmd,%4$cgcc -Wall -Wextra -Werror -o Sully_%6$cd Sully_%6$cd.c%4$c,i,i);%1$c%2$csystem(cmd);%1$c%2$csprintf(name,%4$c./Sully_%6$cd%4$c,i);%1$c%2$cif (i > 0) system(name);%1$c%2$creturn (0);%1$c}%1$c";
 	int fd;
 	char name[10];
+	sprintf(name,"Sully_%d.c",i);
+	if ((fd = open(name, O_RDONLY)) != -1) {--i;close(fd);}
+	char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$cint main()%1$c{%1$c%2$cint i = %3$d;%1$c%2$cint fd;%1$c%2$cchar name[10];%1$c%2$csprintf(name,%4$cSully_%6$cd.c%4$c,i);%1$c%2$cif ((fd = open(name, O_RDONLY)) != -1) {--i;close(fd);}%1$c%2$cchar *s = %4$c%5$s%4$c;%1$c%2$csprintf(name,%4$cSully_%6$cd.c%4$c,i);%1$c%2$cfd = open(name,O_RDWR | O_CREAT, 00644);%1$c%2$cif (fd == -1) exit(1);%1$c%2$cdprintf(fd,s,10,9,i,34,s,37);%1$c%2$cclose(fd);%1$c%2$cchar cmd[100];%1$c%2$csprintf(cmd,%4$cgcc -Wall -Wextra -Werror -o Sully_%6$cd Sully_%6$cd.c%4$c,i,i);%1$c%2$csystem(cmd);%1$c%2$csprintf(name,%4$c./Sully_%6$cd%4$c,i);%1$c%2$cif (i > 0) system(name);%1$c%2$creturn (0);%1$c}%1$c";
 	sprintf(name,"Sully_%d.c",i);
 	fd = open(name,O_RDWR | O_CREAT, 00644);
 	if (fd == -1) exit(1);
