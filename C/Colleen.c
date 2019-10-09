7102a2821a598bcf75244323a5f0a62fdb4ab2bf
Amina MANSOUR <amansour@e1r2p23.42.fr>
2019-10-09 14:32:54 +0200

    Colleen.c fixed

--- C/Colleen.c
+++ C/Colleen.c
@@ -4,14 +4,14 @@
 */
 void print(char *s)
 {
-	printf(s,10,10,9,10,10,10,10,9,10,10,10,10,10,9,10,10,9,34,s,34,10,9,10,9,10,10);
+	printf(s,10,10,9,10,10,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,34,s,34,10,9,10,9,10,10);
 }
 int main()
 {
-/*
-	Hello main.
-*/
-	char *s = "#include <stdio.h>%c/*%c%cHello program.%c*/%cvoid print(char *s)%c{%c%cprintf(s,10,10,9,10,10,10,10,9,10,10,10,10,10,9,10,10,9,34,s,34,10,9,10,9,10,10);%c}%cint main()%c{%c/*%c%cHello main.%c*/%c%cchar *s = %c%s%c;%c%cprint(s);%c%creturn (0);%c}%c";
+	/*
+		Hello main.
+	*/
+	char *s = "#include <stdio.h>%c/*%c%cHello program.%c*/%cvoid print(char *s)%c{%c%cprintf(s,10,10,9,10,10,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,34,s,34,10,9,10,9,10,10);%c}%cint main()%c{%c%c/*%c%c%cHello main.%c%c*/%c%cchar *s = %c%s%c;%c%cprint(s);%c%creturn (0);%c}%c";
 	print(s);
 	return (0);
 }
