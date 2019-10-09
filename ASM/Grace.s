1fe6478fcba9e6540f334ace7318ff10fbb3f868
Amina MANSOUR <amansour@e1r2p23.42.fr>
2019-10-09 14:51:23 +0200

    Sully.s

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -7,7 +7,7 @@ _main:
 push rbp
 mov rbp,rsp
 lea rdi,[rel %1]
-mov rsi,03001o
+mov rsi,SC_FLAGS
 mov rdx,0644o
 mov rax,SC_OPEN
 syscall
@@ -28,11 +28,11 @@ mov rax,0
 ret
 %endmacro
 section .data
-msg: db "; Hello program.%1$c%3$cmacro FT 1%1$cglobal _main%1$cextern _dprintf%1$c%1$c_main:%1$cpush rbp%1$cmov rbp,rsp%1$clea rdi,[rel %3$c1]%1$cmov rsi,03001o%1$cmov rdx,0644o%1$cmov rax,SC_OPEN%1$csyscall%1$cjc .end%1$cmov rdi,rax%1$clea rsi,[rel msg]%1$cmov rdx,10%1$clea rcx,[rel msg]%1$cmov r8,37%1$cmov r9,34%1$ccall _dprintf%1$cmov rax,SC_CLOSE%1$csyscall%1$c.end:%1$cmov rsp,rbp%1$cpop rbp%1$cmov rax,0%1$cret%1$c%3$cendmacro%1$csection .data%1$cmsg: db %4$c%2$s%4$c,0%1$cfilename: db %4$cGrace_kid.s%4$c,0%1$c%3$cdefine SC_OPEN 0x2000005%1$c%3$cdefine SC_CLOSE 0x2000006%1$c%1$csection .text%1$cglobal start%1$c%1$cstart:%1$cFT filename%1$cret%1$c",0
+msg: db "; Hello program.%1$c%3$cmacro FT 1%1$cglobal _main%1$cextern _dprintf%1$c%1$c_main:%1$cpush rbp%1$cmov rbp,rsp%1$clea rdi,[rel %3$c1]%1$cmov rsi,SC_FLAGS%1$cmov rdx,0644o%1$cmov rax,SC_OPEN%1$csyscall%1$cjc .end%1$cmov rdi,rax%1$clea rsi,[rel msg]%1$cmov rdx,10%1$clea rcx,[rel msg]%1$cmov r8,37%1$cmov r9,34%1$ccall _dprintf%1$cmov rax,SC_CLOSE%1$csyscall%1$c.end:%1$cmov rsp,rbp%1$cpop rbp%1$cmov rax,0%1$cret%1$c%3$cendmacro%1$csection .data%1$cmsg: db %4$c%2$s%4$c,0%1$cfilename: db %4$cGrace_kid.s%4$c,0%1$c%3$cdefine SC_OPEN 0x2000005%1$c%3$cdefine SC_CLOSE 0x2000006%1$c%3$cdefine SC_FLAGS 03001o%1$csection .text%1$cglobal start%1$c%1$cstart:%1$cFT filename%1$cret%1$c",0
 filename: db "Grace_kid.s",0
 %define SC_OPEN 0x2000005
 %define SC_CLOSE 0x2000006
-
+%define SC_FLAGS 03001o
 section .text
 global start
 
