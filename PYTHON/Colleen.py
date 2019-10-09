a6a767c1d2b18dad5e6d13609c265052092868e3
Amina MANSOUR <amansour@e1r2p23.42.fr>
2019-10-09 11:31:57 +0200

    Colleen.py fixed

--- PYTHON/Colleen.py
+++ PYTHON/Colleen.py
@@ -1,8 +1,9 @@
-# hello program
+"""hello program
+"""
 
 def print_str(x):
     print(x.format(chr(10),chr(32)*4,chr(34),x))
 
 if __name__ == "__main__":
     # hello main
-    x = "# hello program{0}{0}def print_str(x):{0}{1}print(x.format(chr(10),chr(32)*4,chr(34),x)){0}{0}if __name__ == {2}__main__{2}:{0}{1}# hello main{0}{1}x = {2}{3}{2};print_str(x)";print_str(x)
+    x = "{2}{2}{2}hello program{0}{2}{2}{2}{0}{0}def print_str(x):{0}{1}print(x.format(chr(10),chr(32)*4,chr(34),x)){0}{0}if __name__ == {2}__main__{2}:{0}{1}# hello main{0}{1}x = {2}{3}{2};print_str(x)";print_str(x)
